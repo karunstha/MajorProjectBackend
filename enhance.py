e3ed25e4167f1fe59f435132921679316ba5a8ce
Karun Shrestha <kars.kit35@gmail.com>
2022-03-06 17:40:42 +0545

    worked on image upload

--- enhance.py
+++ enhance.py
@@ -4,9 +4,9 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
+from utils import preprocess_image, save_image
 os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
 
-IMAGE_PATH = "Images.jpg"
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
 
 def enhance(image_path):
@@ -15,7 +15,6 @@ def enhance(image_path):
 
     model = hub.load(SAVED_MODEL_PATH)
 
-    start = time.time()
     fake_image = model(hr_image)
     fake_image = tf.squeeze(fake_image)
 
--- index.py
+++ index.py
@@ -28,9 +28,10 @@ def upload_file():
 		return resp
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
+		extension = filename.rsplit('.', 1)[1].lower()
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		# resp = jsonify({'message' : 'File successfully uploaded'})
-
+		os.rename(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['UPLOAD_FOLDER'], 'latest.'+extension))
+		resp = jsonify({'message' : 'File successfully uploaded'})
 		resp.status_code = 201
 		return resp
 	else:
