d1eb4ddd3f72d5e1f971cd866f46fadff4156a07
Karun Shrestha <kars.kit35@gmail.com>
2022-05-07 17:50:08 +0000

    edits

--- enhance.py
+++ enhance.py
@@ -8,19 +8,20 @@ from utils import preprocess_image, save_image, downscale_image
 from pathlib import Path
 
 os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+SAVED_MODEL_PATH = "C:/Users/Administrator/Documents/Major/tfesr/model"
 
 def enhance(image_path):
-    today = date.today()
     hr_image = preprocess_image(image_path)
     
     # hr_image = downscale_image(tf.squeeze(hr_image))
 
     # save_image(tf.squeeze(fake_image), filename="Downsampled Resolution")
 
-    model = hub.load(SAVED_MODEL_PATH)
-
+    model = hub.KerasLayer(SAVED_MODEL_PATH)
+    
     fake_image = model(hr_image)
     fake_image = tf.squeeze(fake_image)
 
@@ -31,7 +32,7 @@ def enhance_with_downsample(image_path):
     
     hr_image = downscale_image(tf.squeeze(hr_image))
 
-    model = hub.load(SAVED_MODEL_PATH)
+    model = hub.KerasLayer(SAVED_MODEL_PATH)
 
     fake_image = model(hr_image)
     fake_image = tf.squeeze(fake_image)
